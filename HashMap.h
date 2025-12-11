1dab07846a84aa0b642edfcca6e0ed58fc9e07a0
svz71195 <84849514+svz71195@users.noreply.github.com>
2025-12-11 16:20:36 +0100

    Fixed lookup segfault using tombstone

--- HashMap.h
+++ HashMap.h
@@ -5,15 +5,20 @@
 #define INITIAL_CAPACITY 64
 #define FILL_FACTOR 0.75
 
+enum hash_status
+{
+	UNUSED,
+	ISUSED,
+	DELETED
+};
 
 typedef struct
 {
 	char key[KEY_LEN];
 	char value[VAL_LEN];
-	int in_use;
+	short status;
 
 } Entry_t;
-
 
 typedef struct
 {
@@ -23,11 +28,9 @@ typedef struct
 
 } HashMap_t;
 
-
-int map_init(HashMap_t* handle[static 1], size_t capacity);
-void map_free(HashMap_t* m);
-int map_set(HashMap_t* handle[static 1], const char* key, const char* val);
-Entry_t* map_get(HashMap_t* m, const char* key);
-int map_delete(HashMap_t* m, const char* key);
-void map_print(HashMap_t* m);
-
+int map_init(HashMap_t *handle[static 1], size_t capacity);
+void map_free(HashMap_t *m);
+int map_set(HashMap_t *handle[static 1], const char *key, const char *val);
+Entry_t *map_get(HashMap_t *m, const char *key);
+int map_delete(HashMap_t *m, const char *key);
+void map_print(HashMap_t *m);
